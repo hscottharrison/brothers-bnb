7ce4ed2f0ca9ab27484656999063133b22ba8645
Hunter Harrison <hunterharrisondev@gmail.com>
2024-04-29 15:47:10 -0500

    add projects modal

--- public/components/modal.js
+++ public/components/modal.js
@@ -1,39 +1,26 @@
-const contentModalTemplate = document.createElement('template');
-contentModalTemplate.innerHTML = `
+const modalTemplate = document.createElement('template');
+modalTemplate.innerHTML = `
   <link rel="stylesheet" href="../styles/main.css" />
-  <link rel="stylesheet" href="../styles/contentModal.css" />
+  <link rel="stylesheet" href="../styles/modal.css" />
 
   <div class="modal-wrapper">
     <div class="modal-container">
-      <div class="modal-side modal-image">
-        <img />
-      </div>
-      <div class="modal-side modal-content">
         <button class="close-button">
-          <h1>X</h1>
+            <h1>X</h1>
         </button>
-        <h2>
-          <slot name="header" />
-        </h2>
-        <h3>
-          <slot name="subHeader" />
-        </h3>
-        <div class="content">
-          <slot name="content" />
-        </div>
-      </div>
+        <slot name="content" />
     </div>
   </div>
 `
 
-class ContentModal extends HTMLElement {
+class Modal extends HTMLElement {
   open = false;
   wrapper;
   static observedAttributes = ['scroll'];
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(contentModalTemplate.content.cloneNode(true));
+    this.shadowRoot.appendChild(modalTemplate.content.cloneNode(true));
   }
 
   connectedCallback() {
@@ -43,13 +30,11 @@ class ContentModal extends HTMLElement {
   attributeChangedCallback() {
     const scroll = this.getAttribute('scroll');
     const body = document.querySelector('body');
-    const image = this.shadowRoot.querySelector('img');
     if(scroll) {
       this.open = true;
       this.wrapper.classList.add('open');
       this.wrapper.style.top = `${this.getAttribute('scroll')}px`;
       body.style.overflow = 'hidden';
-      image.setAttribute('src', this.getAttribute('image'));
       
       this.shadowRoot.querySelector('.close-button').addEventListener('click', () => {
         this.wrapper.classList.remove('open');
@@ -60,4 +45,4 @@ class ContentModal extends HTMLElement {
 
 }
 
-window.customElements.define('content-modal', ContentModal);
+window.customElements.define('coda-modal', Modal);
--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -1,3 +1,29 @@
+
+function openProjectModal(projectCard) {
+  const modal = document.querySelector('coda-modal');
+
+  const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  modal.setAttribute('scroll', scrollPosition);
+
+  const images = projectCard.images;
+  const photoGrid = document.querySelector('.photo-grid');
+  photoGrid.innerHTML = '';
+  images.forEach((image) => {
+    const img = document.createElement('img');
+    img.setAttribute('src', image);
+    photoGrid.appendChild(img);
+  });
+
+  const projectData = document.querySelector('.project-data');
+  const title = document.querySelector('.project-title');
+  const category = document.querySelector('.project-category');
+  const location = document.querySelector('.project-location');
+  const description = document.querySelector('.project-description');
+  title.innerHTML = projectCard.projectTitle;
+  category.innerHTML = projectCard.projectType;
+  location.innerHTML = projectCard.projectLocation;
+  description.innerHTML = projectCard.projectDescription;
+}
 function renderProjectCards(projectCardData) {
   const projectCardsContainer = document.querySelector('#project-cards-container');
   projectCardData.forEach(projectCard => {
@@ -10,9 +36,8 @@ function renderProjectCards(projectCardData) {
     card.appendChild(title);
 
     card.addEventListener('click', () => {
-      console.log(projectCard.id);
-      card.setAttribute('data', JSON.stringify(projectCard));
-    })
+      openProjectModal(projectCard);
+    });
   
     projectCardsContainer.appendChild(card);
   });
