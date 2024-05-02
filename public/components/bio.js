ddc0ed359450cbf7a00a2dd7ee5f6e6f28164218
Hunter Harrison <hunterharrisondev@gmail.com>
2024-05-02 10:31:38 -0500

    WIP: team page redesign

--- public/components/bio.js
+++ public/components/bio.js
@@ -4,21 +4,35 @@ bioTemplate.innerHTML = `
   <link rel="stylesheet" href="../styles/bio.css" />
 
   <div class="bio-container">
-    <div class="picture-and-title">
-      <div class="tedshot">
-        <slot name="tedshot" />
-      </div>
-      <h3 class="name">
-        <slot name="name" />
-      </h3>
-      <h3 class="title">
-        <slot name="title" /> 
-      </h3>
+    <div class="tedshot">
+      <slot name="tedshot" />
     </div>
-    <p>
-      <slot name="bio" />
-    </p>
-  <div>
+    <h3 class="name">
+    </h3>
+    <h4 class="title">
+    </h4>
+    <span class="bio-button"><strong>BIO ></strong></span>
+  </div>
+
+  <div class="bio-content">
+    <button class="close-button">
+        <h1>X</h1>
+    </button>
+    <div class="bio-content-wrapper">
+      <div class="tedshot">
+        <slot name="tedshot-hidden" />
+      </div>
+      <div>
+        <h3 class="name">
+        </h3>
+        <h4 class="title">
+        </h4>
+        <p>
+          <slot name="bio" />
+        </p>
+      </div>
+    </div>
+  </div>
 `
 
 class Bio extends HTMLElement {
@@ -27,6 +41,29 @@ class Bio extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(bioTemplate.content.cloneNode(true));
   } 
+
+  connectedCallback() {
+    // ATTRIBUTES
+    const nameAttr = this.getAttribute('name');
+    const titleAttr = this.getAttribute('title');
+
+    // ELEMENTS
+    const nameElement = this.shadowRoot.querySelectorAll('.name');
+    const titleElement = this.shadowRoot.querySelectorAll('.title');
+
+    // SET ATTRIBUTES
+    nameElement.forEach(el => el.textContent = nameAttr);
+    titleElement.forEach(el => el.textContent = titleAttr);
+    
+    this.shadowRoot.querySelector('.bio-button').addEventListener('click', () => {
+      this.shadowRoot.querySelector('.bio-content').style.display = 'block';
+      this.shadowRoot.querySelector('.bio-content').style.transition = 'all .3s ease';
+    });
+
+    this.shadowRoot.querySelector('.close-button').addEventListener('click', () => {  
+      this.shadowRoot.querySelector('.bio-content').style.display = 'none';
+    });
+  }
 
 }
 
---
+++ public/scripts/contact.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', async (e) => { 
+  debugger;
+});
